9fd7077d0489e9393d32de65dd551b8e7b3a89d6
Tanner Turba <turba7166@uwlax.edu>
2022-09-21 17:56:39 -0500

    changes
    Signed-off-by: Tanner Turba <turba7166@uwlax.edu>

--- Scheming in Scheme/PrefixEvaluator.java
+++ Scheming in Scheme/PrefixEvaluator.java
@@ -7,15 +7,16 @@ public class PrefixEvaluator
 {
     public PrefixEvaluator(String expression) 
     {
+        // handleOperation('/', new Scanner(" ( + 9 9 ) 8 )"), new Hashtable<>());
         this.expression = expression;
         Scanner scanner = new Scanner(this.expression);
-        System.out.println(rEvaluateExpression(scanner, new Hashtable<>()));
+        System.out.println(EvaluateExpression(scanner, new Hashtable<>(), false));
     }
 
     private String expression;
     Stack<String> operators = new Stack<>();
 
-    private Map<String, String> EvaluateEnvironment(Scanner scanner) 
+    private String evaluateBlock(Scanner scanner) 
     {
         Map<String, String> env = new Hashtable<>();
         Stack<String> parens = new Stack<>();
@@ -33,99 +34,113 @@ public class PrefixEvaluator
             else if(Character.isAlphabetic(token.charAt(0))) 
             {
                 String id = token;
-                String def = rEvaluateExpression(scanner, env);
+                String def = EvaluateExpression(scanner, env, true);
                 env.put(id, def);
             }
         } while(!parens.isEmpty());
-        return env;
+        String result = EvaluateExpression(scanner, env, false);
+        scanner.next();
+        return result;
     }
-
-    public String rEvaluateExpression(Scanner scanner, Map<String, String> environment) 
+    
+    public String EvaluateExpression(Scanner scanner, Map<String, String> environment, boolean evaluatingBlock) 
     {
+        //Get first token, consume if it is '('
         String token = scanner.next();
+        if(token.equals("(")) 
+        {
+            token = scanner.next();
+        }
 
         //Return when an atomic value is reached. 
-        if (Character.isDigit(token.charAt(0)) || /*Character.isAlphabetic(token.charAt(0)) ||*/ token.charAt(0) == '-' && token.length() > 1)
+        if (Character.isDigit(token.charAt(0)) || token.charAt(0) == '-' && token.length() > 1)
         {
             return token;
+        }
+        //Return undefined when undefined
+        else if (token.equals("undefined"))
+        {
+            return "undefined";
         }
         //Opening Parens mark the beginning of a new expression
         else if (token.equals("(")) 
         {
-            return rEvaluateExpression(scanner, environment);
+            return EvaluateExpression(scanner, environment, false);
         }
         //Evaluate the environment when block is used.
         else if (token.toLowerCase().equals("block"))
         {
-            environment = EvaluateEnvironment(scanner);
-            return rEvaluateExpression(scanner, environment);
+            return evaluateBlock(scanner);
         }
         //Return the map value or character when an alphabetic character is found.
         else if (Character.isAlphabetic(token.charAt(0))) 
         {
             String value = environment.get(token);
-            if(value != null)
+            if(value == null)
             {
-                return value;
+                if (evaluatingBlock) 
+                {
+                    return token;
+                }
+                else 
+                {
+                    return "undefined";
+                }
             }
-            return token;
+            return value;
         }
         //Catch operators
         else if (!Character.isDigit(token.charAt(0)) && !Character.isAlphabetic(token.charAt(0)) && token.charAt(0) != '(' && token.charAt(0) != ')' && token.length() == 1)
         {
-            char operator = token.charAt(0);
-            String e1 = rEvaluateExpression(scanner, environment);
-            String e2 = rEvaluateExpression(scanner, environment);
-
-            int exp1, exp2;
-            try 
-            {
-                exp1 = Integer.parseInt(e1);
-                exp2 = Integer.parseInt(e2);
-            }
-            catch(Exception e) 
-            {
-                return "undefined";
-            }
-
-            // Evaluate expression
-            if(token.equals(")"))
-            {
-                if( operator == '+') 
-                {
-                    return exp1 + exp2 + "";
-                } 
-                else if (operator == '-') 
-                {
-                    return exp1 - exp2 + "";
-                } 
-                else if (operator == '*') 
-                {
-                    return exp1 * exp2 + "";
-                } 
-                else if (operator == '/') 
-                {
-                    return exp1 / exp2 + "";
-                } 
-                else if (operator == '^') 
-                {
-                    return (int) Math.pow(exp1, exp2) + "";
-                } 
-                else if (operator == '%') 
-                {
-                    return exp1 % exp2 + "";
-                } 
-                else if (operator == '!') 
-                {
-                    return -exp1 + "";
-                }
-            }
+            return handleOperation(token.charAt(0), scanner, environment);
         }
-        
-        if (token.equals(")") && scanner.hasNext())
+
+        return "";
+    }
+
+    private String handleOperation(char operator, Scanner scanner, Map<String, String> environment) 
+    {
+        String e1 = EvaluateExpression(scanner, environment, false);
+        String e2 = EvaluateExpression(scanner, environment, false);
+
+        int exp1, exp2;
+        try 
         {
-            return rEvaluateExpression(scanner, environment);
+            exp1 = Integer.parseInt(e1);
+            exp2 = Integer.parseInt(e2);
         }
+        catch(Exception e) 
+        {
+            scanner.next();
+            return "undefined";
+        }
+
+        //Consume parens and evaluate expression
+        scanner.next();
+        if( operator == '+') 
+        {
+            return exp1 + exp2 + "";
+        } 
+        else if (operator == '-') 
+        {
+            return exp1 - exp2 + "";
+        } 
+        else if (operator == '*') 
+        {
+            return exp1 * exp2 + "";
+        } 
+        else if (operator == '/') 
+        {
+            return exp1 / exp2 + "";
+        } 
+        else if (operator == '^') 
+        {
+            return (int) Math.pow(exp1, exp2) + "";
+        } 
+        else if (operator == '%') 
+        {
+            return exp1 % exp2 + "";
+        } 
         return "";
     }
 }

c85f3ec6514f4932b8711f4ee665d5895aede8ac
Tanner Turba <turba7166@uwlax.edu>
2022-09-21 13:09:23 -0500

    environment evaluate with iteration
    Signed-off-by: Tanner Turba <turba7166@uwlax.edu>

--- Scheming in Scheme/PrefixEvaluator.java
+++ Scheming in Scheme/PrefixEvaluator.java
@@ -1,10 +1,7 @@
-import java.util.Dictionary;
 import java.util.Hashtable;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Stack;
-
-import javax.xml.crypto.dsig.keyinfo.RetrievalMethod;
 
 public class PrefixEvaluator 
 {
@@ -16,53 +13,31 @@ public class PrefixEvaluator
     }
 
     private String expression;
-    Stack<Dictionary<String, String>> dictionaries = new Stack<>();
     Stack<String> operators = new Stack<>();
-
-    private Map<String, String> addToEnvironment(Map<String, String> environment, Map<String, String> dictionary) {
-
-        //Copy all values into a single map.
-        for (Map.Entry<String, String> entry : dictionary.entrySet()) {
-            environment.put(entry.getKey(), entry.getValue());
-        }
-        return environment;
-    }
 
     private Map<String, String> EvaluateEnvironment(Scanner scanner) 
     {
-        String token = scanner.next();
-        Map<String, String> environment = new Hashtable<>();
-
-        //Get a dictionary of environment.
-        if (token.equals("(")) 
-        {        
-            environment = addToEnvironment(environment, EvaluateEnvironment(scanner));
-            
-            //Consume closing parens if a value was added to the environment
-            if (environment.size() > 0) 
+        Map<String, String> env = new Hashtable<>();
+        Stack<String> parens = new Stack<>();
+        String token;
+        do {
+            token = scanner.next();
+            if(token.equals("(")) 
             {
-                token = scanner.next();
-                if (token.equals("(")) 
-                {
-                    return addToEnvironment(environment, EvaluateEnvironment(scanner));
-                }
+                parens.push(token);
             }
-            return environment;
-        }
-        //A character is found, so it needs to be defined.
-        else if (Character.isAlphabetic(token.charAt(0)))
-        {
-            String id = token;
-            String definition = rEvaluateExpression(scanner, environment);
-            
-            Map<String, String> dictionary = new Hashtable<>();
-            dictionary.put(id, definition);
-            return dictionary;
-        }
-        else 
-        {
-            return environment;
-        }
+            else if(token.equals(")"))
+            {
+                parens.pop();
+            }
+            else if(Character.isAlphabetic(token.charAt(0))) 
+            {
+                String id = token;
+                String def = rEvaluateExpression(scanner, env);
+                env.put(id, def);
+            }
+        } while(!parens.isEmpty());
+        return env;
     }
 
     public String rEvaluateExpression(Scanner scanner, Map<String, String> environment) 
@@ -74,35 +49,26 @@ public class PrefixEvaluator
         {
             return token;
         }
+        //Opening Parens mark the beginning of a new expression
+        else if (token.equals("(")) 
+        {
+            return rEvaluateExpression(scanner, environment);
+        }
         //Evaluate the environment when block is used.
         else if (token.toLowerCase().equals("block"))
         {
             environment = EvaluateEnvironment(scanner);
             return rEvaluateExpression(scanner, environment);
-
-            // return handleBlock(scanner);
         }
-        //Return the map value or undefined when an alphabetic character is found.
+        //Return the map value or character when an alphabetic character is found.
         else if (Character.isAlphabetic(token.charAt(0))) 
         {
-            try 
+            String value = environment.get(token);
+            if(value != null)
             {
-                String value = environment.get(token);
-                if(!value.equals("null"))
-                {
-                    return value;
-                }
-                return "undefined";
+                return value;
             }
-            catch(Exception e) 
-            {
-                return "undefined";
-            }
-        }
-        //Opening Parens mark the beginning of a new expression
-        else if (token.equals("(")) 
-        {
-            return rEvaluateExpression(scanner, environment);
+            return token;
         }
         //Catch operators
         else if (!Character.isDigit(token.charAt(0)) && !Character.isAlphabetic(token.charAt(0)) && token.charAt(0) != '(' && token.charAt(0) != ')' && token.length() == 1)
@@ -121,9 +87,8 @@ public class PrefixEvaluator
             {
                 return "undefined";
             }
-            
-            //Look at the next token. If it is a ')' the expression is over and can be returned.
-            token = scanner.next();
+
+            // Evaluate expression
             if(token.equals(")"))
             {
                 if( operator == '+') 
@@ -163,34 +128,4 @@ public class PrefixEvaluator
         }
         return "";
     }
-
-    private String handleBlock(Scanner scanner) 
-    {
-        Stack<String> parens = new Stack<>();
-        Map<String, String> dictionary = new Hashtable<>();
-        String token = scanner.next();
-        do
-        {
-            if (token.equals("(")) 
-            {
-                parens.push("(");
-            }
-            else if (token.equals(")")) 
-            {
-                parens.pop();
-            }
-            else if (Character.isAlphabetic(token.charAt(0)))
-            {
-                dictionary.put(token, rEvaluateExpression(scanner, dictionary));
-            }
-            
-            if(!parens.isEmpty())
-            {
-                token = scanner.next();
-            }
-        } while (!parens.isEmpty());
-
-        return rEvaluateExpression(scanner, dictionary);  
-    }
-
 }
